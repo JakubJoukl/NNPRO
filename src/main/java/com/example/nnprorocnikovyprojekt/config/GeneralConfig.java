bad12b7431bf464418475310ab136b769235c159
JakubJoukl <st64134@upce.cz>
2024-11-05 18:49:12 +0100

    Fix async

--- src/main/java/com/example/nnprorocnikovyprojekt/config/GeneralConfig.java
+++ src/main/java/com/example/nnprorocnikovyprojekt/config/GeneralConfig.java
@@ -3,11 +3,12 @@ package com.example.nnprorocnikovyprojekt.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAsync
-public class GeneralConfig {
+public class GeneralConfig implements AsyncConfigurer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
