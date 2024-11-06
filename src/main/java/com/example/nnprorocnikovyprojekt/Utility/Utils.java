0abc2fff2f3df82d0b8ed4da509ade1cfb6c3894
JakubJoukl <st64134@upce.cz>
2024-11-06 23:59:53 +0100

    Fix index

--- src/main/java/com/example/nnprorocnikovyprojekt/Utility/Utils.java
+++ src/main/java/com/example/nnprorocnikovyprojekt/Utility/Utils.java
@@ -8,12 +8,12 @@ public class Utils {
 
     }
 
-    public static <T> List<T> getPage(List<T> sourceList, int page, int pageSize) {
-        if(pageSize <= 0 || page <= 0) {
+    public static <T> List<T> getPage(List<T> sourceList, int pageIndex, int pageSize) {
+        if(pageSize <= 0 || pageIndex < 0) {
             throw new IllegalArgumentException("invalid page size: " + pageSize);
         }
 
-        int fromIndex = (page - 1) * pageSize;
+        int fromIndex = (pageIndex) * pageSize;
         if(sourceList == null || sourceList.size() <= fromIndex){
             return Collections.emptyList();
         }
